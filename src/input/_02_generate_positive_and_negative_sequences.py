a3173222a1231a0189b466c5dad416deeae60c81
Soumic <github@gamegame.anonaddy.me>
2024-08-26 08:55:36 +0600

    :hammer: Create positive, and negative datasets

--- src/input/_02_generate_positive_and_negative_sequences.py
+++ src/input/_02_generate_positive_and_negative_sequences.py
@@ -1,26 +1,30 @@
+import time
+
 import pandas as pd
 # import grelu as grelu
 from grelu.data.preprocess import get_gc_matched_intervals
 
 
-if __name__ == "__main__":
-  window = 1000
-  half_window = 500
+def start():
+  half_window = 2500
+  window = 2 * half_window  # 4k ok, 6k not ok
 
-  df = pd.read_csv(
+  df_unfiltered = pd.read_csv(
     "st5_filtered_cosmopolitan_meqtl_snp_cpg_distance_lte_5000.txt",
     sep="\t",
     index_col=0
   )
-  print(df.head())
+  print(df_unfiltered.head())
   #based on this tutorial, I need: https://genentech.github.io/gReLU/tutorials/3_train.html
   positive_seqs: pd.DataFrame = pd.DataFrame()
-  positive_seqs["chrom"] = "chr"+(df["snp.chr"].astype(str))
+  # df = df_unfiltered[df_unfiltered['snp.chr'] == 1]
+  df = df_unfiltered
+  positive_seqs["chrom"] = "chr" + (df["snp.chr"].astype(str))
   positive_seqs["start"] = (df["snp.pos"].astype(int) - half_window)
   positive_seqs["end"] = positive_seqs["start"] + window
   print("--------- positive head -------------")
 
-  positive_seqs = positive_seqs.head(100)
+  # positive_seqs = positive_seqs.head(100)
   print(positive_seqs.head())
 
   # creating negative dataset
@@ -37,5 +41,20 @@ if __name__ == "__main__":
   )
   print("--------- negative head -------------")
   print(negatives.head())
+  positive_seqs.to_csv(f"positives_{window}.csv")
+  negatives.to_csv(f"negatives_{window}.csv")
+  pass
 
+
+if __name__ == "__main__":
+  # Record the start time
+  start_time = time.time()
+  # run code
+  start()
+  # Record the end time
+  end_time = time.time()
+  # Calculate the duration
+  duration = end_time - start_time
+  # Print the runtime
+  print(f"Runtime: {duration:.2f} seconds")
   pass
