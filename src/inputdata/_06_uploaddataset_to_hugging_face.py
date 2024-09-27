8393bc80a989f087610e980f46724a7125e5e3cd
Soumic <github@gamegame.anonaddy.me>
2024-09-27 20:52:46 +0600

    :zap: Create dataset for seq_len = 1000

--- src/inputdata/_06_uploaddataset_to_hugging_face.py
+++ src/inputdata/_06_uploaddataset_to_hugging_face.py
@@ -6,6 +6,10 @@ if __name__ == "__main__":
     "train_binned_200": "dataset_200_train_binned.csv",
     "validate_binned_200": "dataset_200_validate_binned.csv",
     "test_binned_200": "dataset_200_test_binned.csv",
+    # medium samples
+    "train_binned_1000": "dataset_1000_train_binned.csv",
+    "validate_binned_1000": "dataset_1000_validate_binned.csv",
+    "test_binned_1000": "dataset_1000_test_binned.csv",
     # large samples
     "train_binned_4000": "dataset_4000_train_binned.csv",
     "validate_binned_4000": "dataset_4000_validate_binned.csv",
